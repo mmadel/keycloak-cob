24764e2d95b48e50c5d6b7f09670b17d8ad6f7b4
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-08-30 18:45:30 +0300

    change script to parse mysql url

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -40,7 +40,7 @@ file_env 'KEYCLOAK_ADMIN_PASSWORD'
 if [ "$DATABASE_URL" != "" ]; then
   echo "Found database configuration in DATABASE_URL=$DATABASE_URL"
 
-  regex='^postgres://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
+  regex='^mysql://([a-zA-Z0-9_-]+):([a-zA-Z0-9]+)@([a-z0-9.-]+):([[:digit:]]+)/([a-zA-Z0-9_-]+)$'
   if [[ $DATABASE_URL =~ $regex ]]; then
     DB_ADDR=${BASH_REMATCH[3]}
     DB_PORT=${BASH_REMATCH[4]}
