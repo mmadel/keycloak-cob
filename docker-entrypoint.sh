6a0afe8593582cb9b2f6b2d8a97f4cee035f08f5
madel <mohammed.adel.abo.foutouh@gmail.com>
2023-09-07 17:19:44 +0300

    try306

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -71,6 +71,6 @@ SERVER_OPTS="--http-port=$PORT --hostname-strict=false"
 #fi
 
 echo $KEYCLOAK_ADMIN
-exec /opt/keycloak/bin/kc.sh start --http-port=$PORT --auto-build --db=mariadb --proxy=edge --hostname-strict=false --hostname-strict-https=false --db-url jdbc:mysql://dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/wuna1541o83rihlc --db-username n5y21rupsu91iioz --db-password ecylfp4ueqq8l2vd
+exec /opt/keycloak/bin/kc.sh start --http-port=$PORT --auto-build --db=mariadb --proxy=edge --hostname-strict=false --hostname-strict-https=false --db-url jdbc:mysql://dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/wuna1541o83rihlc --db-username g96xausw6n2kfqvd --db-password dxh1jzi27ypcqbqa
 
 exit $?
